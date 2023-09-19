9fe20cc30439c439900e03961d3b95172a7e89f7
Purva Athnere <93878803+purvathnere@users.noreply.github.com>
2023-09-19 17:48:12 +0530

    Rain Water Trapped.cpp

---
+++ Rain Water Trapped.cpp
@@ -0,0 +1,21 @@
+int Solution::trap(const vector<int> &height) {
+    int n = height.size();
+        int mxl[n];
+        mxl[0] = height[0];
+        for(int i=1; i<n; i++){
+            mxl[i] = max(mxl[i-1], height[i]);
+        }
+       
+        int mxr[n];
+        mxr[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            mxr[i] = max(mxr[i+1], height[i]);
+        }
+       
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            int h = abs(min(mxl[i], mxr[i]) - height[i]);
+            ans+=h;
+        }
+        return ans;
+}
