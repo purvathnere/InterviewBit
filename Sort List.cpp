66932b88df89d1024cbe12eb58be70e972e85948
Purva Athnere <93878803+purvathnere@users.noreply.github.com>
2023-09-25 18:33:40 +0530

    Sort List.cpp

---
+++ Sort List.cpp
@@ -0,0 +1,33 @@
+ListNode* Solution::sortList(ListNode* A) {
+    if (A == NULL || A->next == NULL) {
+        return A;
+    }
+
+    // Counting the number of nodes in the list
+    int count = 0;
+    ListNode* curr = A;
+    while (curr != NULL) {
+        count++;
+        curr = curr->next;
+    }
+
+    // Convert the linked list to an array
+    vector<int> arr(count);
+    curr = A;
+    for (int i = 0; i < count; i++) {
+        arr[i] = curr->val;
+        curr = curr->next;
+    }
+
+    // Sort the array
+    sort(arr.begin(), arr.end());
+
+    // Update the linked list with the sorted values
+    curr = A;
+    for (int i = 0; i < count; i++) {
+        curr->val = arr[i];
+        curr = curr->next;
+    }
+
+    return A;
+}
