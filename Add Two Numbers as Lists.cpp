b8e50dc4e16bbab2bae055ed08141d38e15538a6
Purva Athnere <93878803+purvathnere@users.noreply.github.com>
2023-09-30 18:08:40 +0530

    Add Two Numbers as Lists.cpp

---
+++ Add Two Numbers as Lists.cpp
@@ -0,0 +1,24 @@
+ListNode* Solution::addTwoNumbers(ListNode* head1, ListNode* head2) {
+    if(!head1) return head2;
+    if(!head2) return head1;
+    int carry = 0;
+    ListNode* curr = new ListNode(-1);
+    ListNode* temp = curr;
+    while(head1 || head2 || carry){
+        int sum = 0;
+        if(head1){
+            sum += head1->val;
+            head1 = head1->next;
+        }
+        if(head2){
+            sum += head2->val;
+            head2 = head2->next;
+        }
+        sum += carry;
+        carry = sum/10;
+        ListNode* t = new ListNode(sum%10);
+        temp->next = t;
+        temp = temp->next;
+    }
+    return curr->next;
+}
