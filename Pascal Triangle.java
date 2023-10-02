35318e1b57b575336f2a35623158ed709271fb10
Purva Athnere <93878803+purvathnere@users.noreply.github.com>
2023-10-02 16:05:56 +0530

    Create Pascal Triangle.java

---
+++ Pascal Triangle.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public ArrayList<ArrayList<Integer>> solve(int A) {
+   
+    ArrayList<ArrayList<Integer>> ans=new ArrayList<>();
+    if(A==0)return ans;
+    ArrayList<Integer>l=new ArrayList<>();
+    l.add(1);
+    ans.add(l);
+    for(int i=1;i<A;i++){
+        ArrayList<Integer>t=new ArrayList<>();
+        for(int j=0;j<=i;j++){
+            if(j==0||j==i){
+                t.add(1);
+            }
+            else{
+                t.add(ans.get(i-1).get(j-1)+ans.get(i-1).get(j));
+            }
+        }
+        ans.add(t);
+    }    
+    return ans;
+    }
+}
