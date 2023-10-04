aa7938cbcd4059b19e980c6242767760eb30b3ba
Purva Athnere <93878803+purvathnere@users.noreply.github.com>
2023-10-04 22:34:22 +0530

    Longest valid Parentheses.cpp

---
+++ Longest valid Parentheses.cpp
@@ -0,0 +1,17 @@
+int Solution::longestValidParentheses(string A) {
+    int n= A.size();
+    int i=0;
+    stack<pair<char, int>> st;
+    int maxl=0;
+    while(i<n){
+        if(!st.empty() && A[i]==')' && st.top().first == '('){
+            st.pop();
+            if(!st.empty())maxl = max(maxl, i-st.top().second);
+            else maxl= max(maxl, i+1);
+        }else{
+            st.push({A[i], i});
+        }
+        i++;
+    }
+    return maxl;
+}
