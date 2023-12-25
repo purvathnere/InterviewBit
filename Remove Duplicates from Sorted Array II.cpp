8b0fdc2a6822c7cab2dc922740b1ea1cf2974959
Purva Athnere <93878803+purvathnere@users.noreply.github.com>
2023-12-25 19:21:08 +0530

    Create Remove Duplicates from Sorted Array II.cpp

---
+++ Remove Duplicates from Sorted Array II.cpp
@@ -0,0 +1,25 @@
+int Solution::removeDuplicates(vector<int> &A) {
+if(A.size() <= 2) return A.size();
+int l = 0, h = 1;
+while(h<A.size()) {
+if(A[l]!=A[h]) {
+A[++l] = A[h];
+h++;
+continue;
+}
+while(A[l] == A[h] && h<A.size()) {
+h++;
+}
+if(h == A.size()) {
+A[l+1] = A[l];
+return l+2;
+} else {
+A[l+1] = A[l];
+A[l+2] = A[h];
+l = l+2;
+h++;
+}
+}
+return l + 1;
+}
+Reply
