1c503e59fc1cdb7f44605f17aefcf8f329acddfa
Purva Athnere <93878803+purvathnere@users.noreply.github.com>
2023-12-27 22:03:53 +0530

    Create Remove Duplicates from Sorted List.cpp

---
+++ Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,15 @@
+ */
+ListNode* Solution::deleteDuplicates(ListNode* A) {
+    ListNode* curr=A;
+
+    while(curr!=NULL && curr->next!=NULL)
+    {
+        while(curr!=NULL && curr->next!=NULL && curr->val==curr->next->val)
+        {
+            curr->next=curr->next->next;
+        }
+        curr=curr->next;
+    }
+   
+    return A;
+}
